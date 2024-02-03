4e384dfe7da353b98c906c945601124b309d180b
CoderYounes <younesanas2021@gmail.com>
2024-02-03 20:04:18 +0100

    add DELETE Request API

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -5,7 +5,7 @@ from models.state import State
 import models
 from models import storage
 from api.v1.views import app_views
-from flask import jsonify
+from flask import jsonify, abort
 
 
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
@@ -15,3 +15,26 @@ def all_states():
     for state in storage.all(State).values():
         all_states.append(state.to_dict())
     return jsonify(all_states)
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    state = storage.get(State, state_id)
+    if state:
+        return jsonify(state.to_dict())
+    else:
+        abort(404)
+
+
+@app_views.route('/states/<state_id>', methods=['DELETE'])
+def remove_state(state_id):
+    state = storage.get(State, state_id)
+    if state is not None:
+        if state.id == state_id:
+            storage.delete(state)
+            storage.save()
+            return jsonify({}), 200
+        else:
+            abort(404)
+    else:
+        abort(404)
