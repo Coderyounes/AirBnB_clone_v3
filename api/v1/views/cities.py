3cd75a1ac036ca985fef1bfa1937d2d4ddda75c0
CoderYounes <younesanas2021@gmail.com>
2024-02-03 23:24:04 +0100

    GET Method for cities

---
+++ api/v1/views/cities.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" RESTAPI Action For the State Object"""
+
+from models.city import City
+import models
+from models import storage
+from api.v1.views import app_views
+from flask import jsonify, abort, request, make_response
+
+
+@app_views.route('/states/<state_id>/cities/', methods=['GET'],
+                 strict_slashes=False)
+def all_cities(state_id):
+    """ Retreive All States """
+    db_cities = storage.all(City).values()
+    all_cities = []
+    for city in db_cities:
+        if city.id == state_id:
+            all_cities.append(city.to_dict())
+        else:
+            abort(404)
+    return jsonify(all_cities)
