df5ce450d8c073e40a29243479956cb69a7c3681
CoderYounes <younesanas2021@gmail.com>
2024-02-03 17:34:38 +0100

    fix some issue in the stats route

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,6 +4,12 @@ from api.v1.views import app_views
 from flask import jsonify
 from models.engine.db_storage import classes
 import models
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status', strict_slashes=False)
@@ -19,8 +25,10 @@ def stats():
     """
         FUNCTION RETURN CURRENT CLASS STATS
     """
+    classes = [Amenity, City, Place, Review, State, User]
+    db_table = ["amenities", "cities", "places", "reviews", "states", "users"]
     res = {}
-    for key in classes.keys():
-        res[key] = models.storage.count(classes[key])
+    for i in range(len(db_table)):
+        res[db_table[i]] = models.storage.count(classes[i])
 
     return jsonify(res)
