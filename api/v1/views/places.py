df821736f59f62cafe32c139021b62191ec1fd3d
escaper <assaoui54@gmail.com>
2024-02-04 13:04:56 +0100

    feat: place and user class added

---
+++ api/v1/views/places.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+""" RESTAPI Action For the State Object"""
+
+from models.place import Place
+from models import storage
+from api.v1.views import app_views
+from flask import jsonify, abort, request, make_response
+
+
+@app_views.route('/places', methods=['GET'],
+                 strict_slashes=False)
+def all_places():
+    """ Retreive All PLaces """
+    all_places = []
+    for place in storage.all(Place).values():
+        all_places.append(place.to_dict())
+    return jsonify(all_places)
+
+
+@app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
+def get_place(place_id):
+    """ Reteive a Single Place"""
+    place = storage.get(Place, place_id)
+    if place:
+        return jsonify(place.to_dict())
+    else:
+        abort(404)
+
+
+@app_views.route('/places/<place_id>', methods=['DELETE'], strict_slashes=False)
+def remove_place(place_id):
+    """ Remove a Place via their ID"""
+    place = storage.get(Place, place_id)
+    if place is not None:
+        if place.id == place_id:
+            storage.delete(place)
+            storage.save()
+            return jsonify({}), 200
+        else:
+            abort(404)
+    else:
+        abort(404)
+
+
+@app_views.route('/places', methods=['POST'],
+                 strict_slashes=False)
+def create_place():
+    """ Create a new Place"""
+    if not request.json:
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
+    if 'name' not in request.json:
+        return make_response(jsonify({"error": "Missing name"}), 400)
+    new_place = Place(**request.get_json())
+    new_place.save()
+    return jsonify(new_place.to_dict()), 201
+
+
+@app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
+def update_place(place_id):
+    """ Update an Existing Place"""
+    if not request.json:
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    data = request.get_json()
+    if 'name' in data:
+        place.name = data['name']
+    storage.save()
+    return jsonify(place.to_dict()), 200
---
+++ api/v1/views/users.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+""" RESTAPI Action For the State Object"""
+
+from models.user import User
+from models import storage
+from api.v1.views import app_views
+from flask import jsonify, abort, request, make_response
+
+
+@app_views.route('/users', methods=['GET'],
+                 strict_slashes=False)
+def all_users():
+    """ Retreive All PLaces """
+    all_users = []
+    for user in storage.all(User).values():
+        all_users.append(user.to_dict())
+    return jsonify(all_users)
+
+
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
+def get_user(user_id):
+    """ Reteive a Single User"""
+    user = storage.get(User, user_id)
+    if user:
+        return jsonify(user.to_dict())
+    else:
+        abort(404)
+
+
+@app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
+def remove_user(user_id):
+    """ Remove a User via their ID"""
+    user = storage.get(User, user_id)
+    if user is not None:
+        if user.id == user_id:
+            storage.delete(user)
+            storage.save()
+            return jsonify({}), 200
+        else:
+            abort(404)
+    else:
+        abort(404)
+
+
+@app_views.route('/users', methods=['POST'],
+                 strict_slashes=False)
+def create_user():
+    """ Create a new User"""
+    if not request.json:
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
+    if 'name' not in request.json:
+        return make_response(jsonify({"error": "Missing name"}), 400)
+    new_user = User(**request.get_json())
+    new_user.save()
+    return jsonify(new_user.to_dict()), 201
+
+
+@app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
+def update_user(user_id):
+    """ Update an Existing User"""
+    if not request.json:
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    data = request.get_json()
+    if 'name' in data:
+        user.name = data['name']
+    storage.save()
+    return jsonify(user.to_dict()), 200
