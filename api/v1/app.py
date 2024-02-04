0df0fe31c774db0c56bd63ef604e8e4a59a874f4
CoderYounes <younesanas2021@gmail.com>
2024-02-05 00:46:36 +0100

    add the CORS instance

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,10 +6,12 @@ from os import environ
 from api.v1.views import app_views
 from werkzeug.exceptions import NotFound
 from flask import jsonify
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.errorhandler(NotFound)
